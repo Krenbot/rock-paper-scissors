c8c1715388ff7fb4f2ecd3184db126434402fca5
Steven Krenn <stevekrenn@gmail.com>
2022-09-12 19:37:06 -0500

    added psuedocode for game

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-console.log("yo!")
+//define array for choices [r, p, s]
+//define var for computerChoice
+//define var for playerChoice
+//define var for wins
+//define var for losses
+//define var for ties
+
+//prompt for playerChoice
+
+//randomly choose computerChoice
+
+//compare choices
+    //if playerChoice is 'p' and computerChoice is 'r' 
+
+//display (alert) results (win/tie/lose round)
+
+//show stats (number of wins/losses/ties)
+
+//play again? y/n
+    //restart game
+    //else, 
+    //cancel game
